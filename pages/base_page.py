6fe7a35302d33ab9611399f764d4b892909e6829
MaksimFilatov <Fillife.one@ya.ru>
2025-08-14 16:04:10 +0300

    рефакторинг: добавлен базовый класс для вызова методов в разные страницы

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        self.action = ActionChains(self.driver)
