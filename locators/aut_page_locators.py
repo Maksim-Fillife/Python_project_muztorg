363981c93026cde2cbc1717897c65e9b0f21c995
MaksimFilatov <Fillife.one@ya.ru>
2025-08-14 16:27:54 +0300

    Добавлен класс с локаторами для авторизации

---
+++ locators/aut_page_locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+
+class AutLocators:
+
+    INPUT_BUTTON = (By.XPATH, "//div[@class='header-auth']")
+    LOGIN_WITH_PASSWORD_LINK = (By.CSS_SELECTOR, "button.js-change-mode-to-login-by-password[data-mode='login-by-password']")
+    INPUT_EMAIL = (By.XPATH, "//input[@id='login-by-pass_phone-or-email-input']")
+    INPUT_PASSWORD = (By.XPATH, "//input[@id='login-by-pass_password-input']")
+    SUBMIT_BUTTON = (By.XPATH, "//button[@class='button button-orange _large js-login-form-submit-btn']")
+    PROFILE_BUTTON = (By.XPATH, "//div[contains(@class,'header-auth__menu')]//a[@class='mt-header__link']")
+    ERROR_MESSAGE = (By.XPATH, "//div[@class='error-message']")
